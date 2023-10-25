bb65e2edbba9bddcb1881b5ca9342220b4bd82da
ridwaanhall <erbyl14@gmail.com>
2023-10-25 07:21:27 +0700

    first commit

---
+++ 04_07/04_07_begin.py
@@ -0,0 +1,20 @@
+'''
+
+Simple Neural Network using Keras
+Introduction
+Our use case is to build, train, and evaluate a prediction model for sales analysis.
+
+In this model, we need to feed the advertising budget of TV, radio, and newspapers to the model and the model will forecast the possible sales.
+
+Dataset
+The advertising dataset captures the sales revenue generated with respect to advertisement costs across numerous platforms like radio, TV, and newspapers.
+
+Features:
+Digital: advertising dollars spent on Internet.
+TV: advertising dollars spent on TV.
+Radio: advertising dollars spent on Radio.
+Newspaper: advertising dollars spent on Newspaper.
+Target (Label):
+Sales budget
+
+'''
